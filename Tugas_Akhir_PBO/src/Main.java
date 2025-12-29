e227da0ed481cb74e00444397fd22a7b1b1a4dc2
Muhammad Alfaraby <alfaraby995@gmail.com>
2025-12-29 23:34:57 +0700

    Delete Tugas_Akhir_PBO directory

--- Tugas_Akhir_PBO/src/Main.java
+++
@@ -1,7 +0,0 @@
-import ui.LoginFrame;
-
-public class Main {
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(() -> new LoginFrame().setVisible(true));
-    }
-}
