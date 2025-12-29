8571d497848e6b1853e56eeb1161d16fabb4353e
padilarkannn <padilarkannn@gmail.com>
2025-12-29 23:57:29 +0700

    menambahkan src

---
+++ PBO_TA/src/model/Admin.java
@@ -0,0 +1,39 @@
+package model;
+
+public class Admin {
+    private int id;
+    private String username, passwordHash;
+
+    public Admin() {
+    }
+
+    public Admin(int id, String username, String passwordHash) {
+        this.id = id;
+        this.username = username;
+        this.passwordHash = passwordHash;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+}
